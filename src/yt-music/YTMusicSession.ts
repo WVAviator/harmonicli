f6ad86e489e2194a60eb315caa44b24df0e1e277
Wvaviator <wvaviator@gmail.com>
2022-10-02 13:37:59 -0500

    add function to subscribe to song updates

--- src/yt-music/YTMusicSession.ts
+++ src/yt-music/YTMusicSession.ts
@@ -13,7 +13,19 @@ import {
 const YOUTUBE_MUSIC_URL = 'https://music.youtube.com/';
 
 export class YTMusicSession {
-  private constructor(private page: Page) {}
+  private playUpdateSubscribers = [];
+
+  private currentSong: string = '';
+  private currentArtist: string = '';
+
+  public get nowPlaying() {
+    return `${this.currentSong} | ${this.currentArtist}`;
+  }
+
+  private constructor(private page: Page) {
+    this.subscribePlayUpdates((song) => console.log('Now playing: ', song));
+    this.handlePlayUpdate();
+  }
 
   static async create(
     args?: string[],
@@ -66,5 +78,62 @@ export class YTMusicSession {
     const nextSongSelector = `tp-yt-paper-icon-button[title="Next song"]`;
     await this.page.waitForSelector(nextSongSelector);
     await this.page.click(nextSongSelector);
+  }
+
+  public async previousSong() {
+    const previousSongSelector = `tp-yt-paper-icon-button[title="Previous song"]`;
+    await this.page.waitForSelector(previousSongSelector);
+    await this.page.click(previousSongSelector);
+  }
+
+  public async playPause() {
+    const playPauseSelector = `#play-pause-button`;
+    await this.page.waitForSelector(playPauseSelector);
+    await this.page.click(playPauseSelector);
+  }
+
+  public subscribePlayUpdates = (callback: (nowPlaying: string) => void) => {
+    this.playUpdateSubscribers.push(callback);
+  };
+
+  private async handlePlayUpdate() {
+    await this.page.exposeFunction(
+      'handlePlayUpdate',
+      (newSongInfo: string, newArtistInfo: string) => {
+        this.currentSong = newSongInfo;
+        this.currentArtist = newArtistInfo;
+        this.playUpdateSubscribers.forEach((subscriber) =>
+          subscriber(this.nowPlaying)
+        );
+      }
+    );
+    await Promise.all([
+      this.page.waitForSelector(`ytmusic-player-bar yt-formatted-string`),
+      this.page.waitForSelector(
+        `ytmusic-player-bar span.subtitle yt-formatted-string a`
+      ),
+    ]);
+
+    await this.page.evaluate(() => {
+      const observer = new MutationObserver(() => {
+        const newSongElement: HTMLElement = document.querySelector(
+          `ytmusic-player-bar yt-formatted-string`
+        );
+        const newArtistElement: HTMLElement = document.querySelector(
+          `ytmusic-player-bar span.subtitle yt-formatted-string a`
+        );
+
+        //@ts-ignore
+        handlePlayUpdate(newSongElement.innerText, newArtistElement.innerText);
+      });
+      observer.observe(
+        document.querySelector(`ytmusic-player-bar yt-formatted-string`),
+        {
+          attributes: false,
+          childList: true,
+          subtree: true,
+        }
+      );
+    });
   }
 }
