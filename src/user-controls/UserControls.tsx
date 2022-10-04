14b40258c6ba6af1570c89575ea95a31eb5f6dc3
Wvaviator <wvaviator@gmail.com>
2022-10-04 18:19:50 -0500

    refactored usercontrols to a functional component

--- src/user-controls/UserControls.tsx
+++ src/user-controls/UserControls.tsx
@@ -1,47 +1,61 @@
 import { BrowserSession } from './BrowserSession';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Text } from 'ink';
 import SelectInput from 'ink-select-input';
+import { BrowserSessionContext } from '../BrowserSessionProvider';
 
-export class UserControls extends React.Component {
-  session: BrowserSession;
+const UserControls = () => {
+  // session: BrowserSession;
 
-  constructor(props) {
-    super (props);
+  // constructor(props) {
+  //   super (props);
 
-    this.session = props.session;
+  //   this.session = props.session;
 
-    this.state = {
-      nowPlaying: this.session.PlayUpdates.nowPlaying,
-    }
-    
-    this.session.PlayUpdates.subscribe((song) => {
-      this.updateSongTitle(song);
+  //   this.state = {
+  //     nowPlaying: this.session.PlayUpdates.nowPlaying,
+  //   }
+
+  //   this.session.PlayUpdates.subscribe((song) => {
+  //     this.updateSongTitle(song);
+  //   });
+
+  // }
+
+  // async updateSongTitle(song: string) {
+  //   this.setState({
+  //     nowPlaying: song,
+  //   })
+  // }
+
+  // getChoices() {
+  //   return [...this.session.PlaybackControls.controlActions];
+  // }
+
+  // handleAction (action) {
+  //   action.value();
+  // }
+  console.log('Rendering UserControls');
+
+  const session = useContext(BrowserSessionContext);
+  const [nowPlaying, setNowPlaying] = useState('');
+
+  useEffect(() => {
+    session.PlayUpdates.subscribe((nowPlaying) => {
+      setNowPlaying(nowPlaying);
     });
+  }, []);
 
-  }
+  return (
+    <>
+      <Text>{nowPlaying}</Text>
+      <Text>Test</Text>
+      <SelectInput
+        items={session.PlaybackControls.controlActions}
+        onSelect={(item) => item.value()}
+      ></SelectInput>
+    </>
+  );
+};
 
-  async updateSongTitle(song: string) {
-    this.setState({
-      nowPlaying: song,
-    })
-  }
-
-  getChoices() {
-    return [...this.session.PlaybackControls.controlActions];
-  }
-
-  handleAction (action) {
-    action.value();
-  }
-
-  render () {
-    return (
-      <>
-        {/* <Text>{this.state.nowPlaying}</Text> */}
-        <Text>Test</Text>
-        <SelectInput items={this.getChoices()} onSelect={this.handleAction}></SelectInput>
-      </>
-    )
-  }
-}
+export default UserControls;
