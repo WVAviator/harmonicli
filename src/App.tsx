c6c3b1ee358db188438807133fb944a154abe2a1
Wvaviator <wvaviator@gmail.com>
2022-10-04 18:32:21 -0500

    removed debug logs

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ export const App: React.FC<AppProps> = ({ args = [], options = {} }) => {
   const [session, setSession] = useState(null);
 
   useEffect(() => {
-    console.log('Establishing session...');
     const establishSession = async () => {
       //TODO: Un-hardcode YTMusicSession here so that SpotifySessions can be instantated as well.
       const newSession = await YTMusicSession.create(args, options);
@@ -23,9 +22,8 @@ export const App: React.FC<AppProps> = ({ args = [], options = {} }) => {
 
     establishSession();
     return () => {
-      console.log('Terminating session...');
       const terminateSession = async () => {
-        await session.close();
+        await session?.close();
         setSession(null);
       };
       terminateSession();
--- src/user-controls/UserControls.tsx
+++ src/user-controls/UserControls.tsx
@@ -1,42 +1,9 @@
-import { BrowserSession } from './BrowserSession';
 import React, { useContext, useEffect, useState } from 'react';
 import { Text } from 'ink';
 import SelectInput from 'ink-select-input';
 import { BrowserSessionContext } from '../BrowserSessionProvider';
 
 const UserControls = () => {
-  // session: BrowserSession;
-
-  // constructor(props) {
-  //   super (props);
-
-  //   this.session = props.session;
-
-  //   this.state = {
-  //     nowPlaying: this.session.PlayUpdates.nowPlaying,
-  //   }
-
-  //   this.session.PlayUpdates.subscribe((song) => {
-  //     this.updateSongTitle(song);
-  //   });
-
-  // }
-
-  // async updateSongTitle(song: string) {
-  //   this.setState({
-  //     nowPlaying: song,
-  //   })
-  // }
-
-  // getChoices() {
-  //   return [...this.session.PlaybackControls.controlActions];
-  // }
-
-  // handleAction (action) {
-  //   action.value();
-  // }
-  console.log('Rendering UserControls');
-
   const session = useContext(BrowserSessionContext);
   const [nowPlaying, setNowPlaying] = useState('');
 
@@ -49,7 +16,6 @@ const UserControls = () => {
   return (
     <>
       <Text>{nowPlaying}</Text>
-      <Text>Test</Text>
       <SelectInput
         items={session.PlaybackControls.controlActions}
         onSelect={(item) => item.value()}
