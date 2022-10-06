128dab2a0ceea77d232fa69e29697449dd6d027d
Wvaviator <wvaviator@gmail.com>
2022-10-05 19:55:42 -0500

    added unsubscribe functionality for PlayUpdates

--- src/user-controls/PlayUpdates.ts
+++ src/user-controls/PlayUpdates.ts
@@ -2,6 +2,7 @@ export type PlayUpdateSubscriber = (nowPlaying: string) => void;
 
 export interface PlayUpdates {
   nowPlaying: string;
-  subscribe: (callback: PlayUpdateSubscriber) => void;
+  subscribe: (callback: PlayUpdateSubscriber) => string;
+  unsubscribe: (subscriberId: string) => void;
   forceSongUpdate: () => void;
 }
