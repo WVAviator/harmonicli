0a71ce601674445bb47405e38bfd620bfcdbed27
Wvaviator <wvaviator@gmail.com>
2022-10-07 14:48:58 -0500

    add documentation to abstract interfaces

--- src/user-controls/BrowserSession.ts
+++ src/user-controls/BrowserSession.ts
@@ -2,6 +2,9 @@ import { ProgressUpdate } from '../progress-bar/ProgressUpdate';
 import { PlaybackControls } from './PlaybackControls';
 import { PlayUpdates } from './PlayUpdates';
 
+/**
+ * An abstract interface to be implemented by any class that provides a session for the user interface to display and control.
+ */
 export interface BrowserSession {
   search: (args: string[]) => Promise<void>;
   PlaybackControls: PlaybackControls;
--- src/user-controls/PlayUpdates.ts
+++ src/user-controls/PlayUpdates.ts
@@ -1,5 +1,8 @@
 export type PlayUpdateSubscriber = (nowPlaying: string) => void;
 
+/**
+ * An abstract interface to be implemented by a class that manages subscriptions to the currently playing song and its updates.
+ */
 export interface PlayUpdates {
   nowPlaying: string;
   subscribe: (callback: PlayUpdateSubscriber) => string;
