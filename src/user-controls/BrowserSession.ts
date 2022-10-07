f1898ae46e3bb802e03a7b461ac55266228921af
Wvaviator <wvaviator@gmail.com>
2022-10-07 14:56:57 -0500

    add more abstract interface docs

--- src/user-controls/BrowserSession.ts
+++ src/user-controls/BrowserSession.ts
@@ -6,9 +6,15 @@ import { PlayUpdates } from './PlayUpdates';
  * An abstract interface to be implemented by any class that provides a session for the user interface to display and control.
  */
 export interface BrowserSession {
+  /**
+   * This function should execute a search based on the passes in arguments.
+   */
   search: (args: string[]) => Promise<void>;
   PlaybackControls: PlaybackControls;
   PlayUpdates: PlayUpdates;
   ProgressUpdates: ProgressUpdate;
+  /**
+   * This function should terminate the session.
+   */
   close: () => Promise<void>;
 }
--- src/user-controls/PlayUpdates.ts
+++ src/user-controls/PlayUpdates.ts
@@ -4,8 +4,20 @@ export type PlayUpdateSubscriber = (nowPlaying: string) => void;
  * An abstract interface to be implemented by a class that manages subscriptions to the currently playing song and its updates.
  */
 export interface PlayUpdates {
+  /**
+   * THis should return a string formatted to display the song and artist.
+   */
   nowPlaying: string;
+  /**
+   * This should allow subscriptions to nowPlaying updates and return a unique identifier.
+   */
   subscribe: (callback: PlayUpdateSubscriber) => string;
+  /**
+   * This should remove a callback from subscriptions based on its unique identifier.
+   */
   unsubscribe: (subscriberId: string) => void;
+  /**
+   * This should manually invoke a song update.
+   */
   forceSongUpdate: () => void;
 }
