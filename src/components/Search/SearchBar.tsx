813c305728fc16df0bb7c52fc8dc30ce9c958c51
Mathew Hultquisr <mathew@gates.services>
2022-10-10 14:31:04 -0400

    Basic search results/searching functionality

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import TextInput from 'ink-text-input';
 import { Text, Box, useFocus, useInput, useFocusManager } from 'ink';
 import { BrowserSessionContext } from '../../BrowserSessionProvider';
@@ -6,48 +6,62 @@ import { SearchResults } from './SearchResults';
 
 export const SearchBar = () => {
   const { isFocused } = useFocus({ id: 'search-bar'});
-  const { focusNext, focusPrevious, focus } = useFocusManager();
+  const { focus } = useFocusManager();
 
   const session = useContext(BrowserSessionContext);
   const [inputValue, setInputValue] = useState('');
+  const [ searchResultActive, setSearchResultActive ] = useState(false);
 
   useInput((_, key) => {
-    if (!isFocused) return;
+    if (!isFocused || searchResultActive) return;
 
-    if (key.upArrow && isFocused) {
+    if (key.upArrow) {
+      setSearchResultActive(false);
       focus('playback-controls');
     }
-    if (key.downArrow && isFocused) {
-      focus('search-selector');
-    }
+    // if (key.downArrow) {
+    //   focus('search-selector');
+    //   setSearchResultActive(true);
+    // }
   });
 
   const handleSearchSubmit = function (query: string) {
     focus('search-selector');
+    setSearchResultActive(true);
     session.SearchHandler.search(query);
   }
 
-  // const focusedElements = () => {
-  //   if (isFocused) {
-  //     return <TextInput placeholder='Search' value={inputValue} onChange={setInputValue} onSubmit={handleSearchSubmit}/>
-  //   } else {
+  // if (searchResultActive) focus('search-selector');
 
-  //   }
-  // }
+  const view = () => {
+    if (searchResultActive) {
+      return (
+        <Box>
+          <SearchResults state={{searchResultActive, setSearchResultActive}}/>
+        </Box>
+      );
+    }
+    if (!isFocused) {
+      return (
+        <Box>
+          <Text>Search 🔎</Text>
+        </Box>
+      );
+    }
+
+    return (
+      <Box>
+        <TextInput placeholder='Search 🔎' value={inputValue} onChange={setInputValue} onSubmit={handleSearchSubmit}/>
+      </Box>
+    );
+
+  }
 
   return (
     <Box 
       borderStyle="round"
-      borderColor={isFocused ? 'red' : 'white'}
-    >
-      {/* User search */}
-      <Box>
-        {isFocused ? <TextInput placeholder='Search' value={inputValue} onChange={setInputValue} onSubmit={handleSearchSubmit}/> : <Text>Search</Text>}
-      </Box>
-      {/* Search Results */}
-      <Box>
-        <SearchResults />
-      </Box>
+      borderColor={isFocused ? 'red' : 'white'}>
+      {view()}
     </Box>
   )
 }
--- src/components/Search/SearchResults.tsx
+++ src/components/Search/SearchResults.tsx
@@ -1,32 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, FC } from "react";
 import useSongList from "../../hooks/useSongList";
 import { BrowserSessionContext } from "../../BrowserSessionProvider";
 import SelectInput from "ink-select-input/build";
 import { Text, useFocus, useFocusManager, useInput } from "ink";
+import Gradient from 'ink-gradient';
+import Spinner from 'ink-spinner';
 
-export const SearchResults = () => {
+type SRState = {
+  searchResultActive: boolean,
+  setSearchResultActive: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+export const SearchResults:FC<{state: SRState}> = ({state}) => {
 
   const session = useContext(BrowserSessionContext);
 
   const songList = useSongList();
 
   const { isFocused } = useFocus({id: 'search-selector'});
-  const { focusNext, focusPrevious, focus } = useFocusManager();
+  const { focus } = useFocusManager();
+  // const [ searchResultActive, setSearchResultActive ] = useSearchResultsActive();
 
   useInput ((_, key) => {
-    if (!isFocused) return;
+    // if (!isFocused) return;
+    if (!state.searchResultActive) return;
 
     if (key.leftArrow) {
-      // focusPrevious();
+      state.setSearchResultActive(false);
       focus('search-bar');
     }
     if (key.rightArrow) {
-      // focusNext();
+      // do nothing.
     }
   })
 
   const handleSelect = (selection) => {
-    focus('search-bar');
+    state.setSearchResultActive(false);
+    focus('playback-controls');
     session.SearchHandler.play(selection.value);
   }
 
@@ -37,17 +47,30 @@ export const SearchResults = () => {
     })
   });
 
-  return (
-    <>
-      {songList && isFocused ? 
-        (
+  // console.log(`Search res active: ${searchResultActive}`);
+  // if (!state.searchResultActive) focus('search-bar');
+
+  if (state.searchResultActive) {
+    if (songList?.at(0) !== undefined) {
+      return (
+        <>
           <SelectInput items={parsedSongSelections} onSelect={handleSelect} />
-        )
-        : 
-        (
-          <></>
-        )
-      }
-    </>
-  )
+        </>
+      );
+    }
+    return (
+      <Text>
+      <Gradient name="summer">
+        <Text>
+          <Spinner type="bouncingBall" />
+        </Text>
+      </Gradient>
+      {' Loading Results'}
+    </Text>
+    );
+  }
+
+  return (
+    <></>
+  );
 }
