1a73a306cd47cbc3cfa1f323e8b823fc1a15a5ad
Mathew Hultquist <mathew@gates.services>
2022-10-18 11:52:43 -0400

    Began adding options component

--- src/components/Options/Options.tsx
+++ src/components/Options/Options.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, useFocus, useFocusManager } from 'ink';
+import ConfigUtils, { Config } from "../../utils/ConfigUtility";
 
 const Options = () => {
+    const config = ConfigUtils.getConfig();
+    const { isFocused } = useFocus();
+    const { focusNext, focusPrevious } = useFocusManager();
+    
+    // Will re-write this a little later.
+    const [ menuOptions, setMenuOptions ] = useState(Object.keys(config).map(item => { return { label: item, value: () => subMenuOptions(config[item]) } }));
 
+    // const defaultConfig: Config = {
+    //     'colors': {
+    //       textColor: 'white',
+    //       focusColor: 'yellow',
+    //       unfocusedColor: 'white',
+    //       gradient: 'summer',
+    //     },
+    //     'defaultPlayer': 'YTMusic',
+    //   }
+    
+    const subMenuOptions = (item) => {
+        setMenuOptions(Object.entries(item).map(item => { return { label: item[0], value: () => console.log(item[1]) } }));
+    }
+
+
+    return (
+        <Box>
+
+        </Box>
+    );
 }
