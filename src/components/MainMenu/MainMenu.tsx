132a50eaf540af8f35b68da2f2518d3b2144769f
Mathew Hultquist <mathew@gates.services>
2022-11-12 11:42:37 -0500

    Some refactoring and added custom focus state

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,5 +1,6 @@
 import React, { Children, createContext, FC, ReactElement, ReactNode, useState } from "react";
 import { Box, Text, useFocus, useFocusManager, useInput } from 'ink';
+import { KeyObject } from "crypto";
 
 export type Item = {
     label: string, // The label for the item.
@@ -7,61 +8,234 @@ export type Item = {
     action?: (id: number) => void, // An optional function to call when the item is selected.
 }
 
-const MainMenu: FC<{ items: Item[], children?: any }> = ({ items, children }) => {
-    const [ selected, setSelected ] = useState({ id: 0, focused: false, childrenSelected: false });
-    const { isFocused } = useFocus({ id: 'main-menu' })
-    const { focus, focusNext, focusPrevious } = useFocusManager();
+interface SelectedState {
+    selectedStateHelper: (state: State) => State,
+}
 
+export type State = {
+    buttonId: number,
+    childId: number,
+    buttonFocused: boolean,
+    childBoxFocused: boolean,
+    buttonBoxFocused: boolean,
+}
+
+// This is only here so we can have default values in the state initilizer. Couldn't figure out another way.
+type StateUnknown = {
+    buttonId?: number,
+    childId?: number,
+    buttonFocused?: boolean,
+    childBoxFocused?: boolean,
+    buttonBoxFocused?: boolean,
+}
+
+const MainMenu: FC<{ items: Item[], children?: any, defaultChildFocused?: number }> = ({ items, children, defaultChildFocused }) => {
+
+    const defaultState: State = {
+        buttonId: 0,
+        childId: Array.isArray(children) ? defaultChildFocused ?? 0 : 0,
+        buttonFocused: false,
+        childBoxFocused: true,
+        buttonBoxFocused: false,
+    };
+
+    const [ selected, setSelected ] = useState<State>(defaultState);
+
+    const selectedStateHelper = ({ buttonId, childId, buttonFocused, childBoxFocused, buttonBoxFocused }: State | StateUnknown ): State => {
+        return ({
+            buttonId: buttonId ?? selected.buttonId,
+            childId: childId ?? selected.childId,
+            buttonFocused: buttonFocused ?? selected.buttonFocused,
+            childBoxFocused: childBoxFocused ?? selected.childBoxFocused,
+            buttonBoxFocused: buttonBoxFocused ?? selected.buttonBoxFocused,
+        });
+    }
+    // const { isFocused } = useFocus({ id: 'main-menu' });
+    // const { focus, focusNext, focusPrevious } = useFocusManager();
+
+
+    // This can be refactored. TODO: refactor
     useInput((_, key) => {
-        if (!isFocused) return;
+        // Child box logic
+        if (selected.childBoxFocused) {
+            if (key.upArrow) {
 
-        // If we're at the top row, go to the previous focus.
-        if (key.upArrow && selected.id <= 2) {
-            focusPrevious();
-            return;
+                // If we are the top of the selected children, go to buttons
+                if (selected.childId <= 0 && selected.childBoxFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonBoxFocused: true,
+                        childBoxFocused: false,
+                        childId: Array.isArray(children) ? children.length - 1 : 0,
+                    }));
+                    return;
+                }
+
+                // Reset childId to last child if it goes out of bounds
+                if (Array.isArray(children) && selected.childId < 0) {
+                    setSelected(selectedStateHelper({
+                        childId: 0,
+                    }));
+                    return;
+                }
+
+                // childID should always be 0 if there is only one
+                if (!Array.isArray(children)) {
+                    return;
+                }
+
+                // Select previous child
+                setSelected(selectedStateHelper({
+                    childId: selected.childId - 1,
+                }));
+                
+            }
+            
+            if (key.downArrow) {
+                // Shift focus to the button box if we run out of children.
+                if (selected.childId >= children?.length - 1 && selected.childBoxFocused) {
+                    setSelected(selectedStateHelper({
+                        childBoxFocused: false,
+                        buttonBoxFocused: true,
+                    }));
+                    return;
+                }
+
+                // Reset childId to last child if it goes out of bounds
+                if (Array.isArray(children) && selected.childId >= children.length) {
+                    setSelected(selectedStateHelper({
+                        childId: children.length - 1,
+                    }));
+                    return;
+                }
+
+                // childID should always be 0 if there is only one
+                if (!Array.isArray(children)) {
+                    return;
+                }
+
+                // Select next child
+                setSelected(selectedStateHelper({
+                    childId: selected.childId + 1,
+                }));
+
+            }
         }
 
-        // If we're at the bottom row, go to next focus
-        if (key.downArrow && selected.id >= items.length-2 ) {
-            focusNext();
-            return;
-        }
+        // Button box logic
+        if (selected.buttonBoxFocused) {
 
-        // Go up to the item above the current item.
-        if (key.upArrow) {
-            if (selected.id - 3 < 0 || selected.focused) return;
-            setSelected({ id: selected.id - 3, focused: false, childrenSelected: false, });
-        }
+            if (key.upArrow) {
 
-        // Go down to the item below the current item.
-        if (key.downArrow) {
-            if (selected.id + 3 >= items.length || selected.focused) return;
-            setSelected({ id: selected.id + 3, focused: false, childrenSelected: false, });
-        }
+                // // If the selected buttonId is out of range, reset to 0
+                // if (selected.buttonId - 3 < 0) {
+                //     setSelected(selectedStateHelper({
+                //         buttonId: 0,
+                //     }));
+                //     return;
+                // }
 
-        // Go to the previous item
-        if (key.leftArrow) {
-            if (selected.id === 0 || selected.focused) return;
-            setSelected({ id: selected.id-1, focused: false, childrenSelected: false, });
-        }
+                // If we're at the top row, go to child box
+                if (selected.buttonId <= 2 && !selected.buttonFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonBoxFocused: false,
+                        childBoxFocused: true,
+                    }));
+                    return;
+                }
 
-        // Go to the next item
-        if (key.rightArrow) {
-            if (selected.id === items.length-1 || selected.focused) return;
-            setSelected({ id: selected.id+1, focused: false, childrenSelected: false, });
-        }
+                // Go up to the item above the current item.
+                if (selected.buttonId - 3 !< 0 && !selected.buttonFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonId: selected.buttonId - 3,
+                    }));
+                    return;
+                }
 
-        // Leave the focused item
-        if (key.escape && selected.focused) {
-            setSelected( { id: selected.id, focused: false, childrenSelected: false, } );
-            // Sometimes this component gets unfocused when pressing escape. That's what this is for.
-            focus('main-menu');
-        }
+            }
 
-        // Enter/leave the focused item.
-        if (key.return && !selected.focused) {
-            items[selected.id].action(selected.id);
-            setSelected( { id: selected.id, focused: !selected.focused, childrenSelected: false } );
+            if (key.downArrow) {
+
+                // If the selected buttonId is out of range, reset it to last index
+                // if (selected.buttonId + 3 >= items.length) {
+                //     setSelected(selectedStateHelper({
+                //         buttonId: items.length - 1,
+                //     }));
+                //     return;
+                // }
+
+                // If we're at the bottom row, go to child box
+                if (selected.buttonId >= items.length-2 && !selected.buttonFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonBoxFocused: false,
+                        childBoxFocused: true,
+                        childId: 0,
+                    }));
+                    return;
+                }
+
+                // Go down to the item below the current item.
+                if (selected.buttonId + 3 !>= items.length && !selected.buttonFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonId: selected.buttonId + 3,
+                    }));
+                    return;
+                }
+
+
+
+            }
+
+            if (key.leftArrow) {
+
+                // Go to the previous item
+                if (selected.buttonId !== 0 && !selected.buttonFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonId: selected.buttonId-1,
+                    }));
+                    return;
+                }
+
+            }
+
+            if (key.rightArrow) {
+
+                // Go to the next item
+                if (selected.buttonId !== items.length-1 && !selected.buttonFocused) {
+                    setSelected(selectedStateHelper({
+                        buttonId: selected.buttonId + 1,
+                    }));
+                    return;
+                }
+            }
+
+            if (key.return) {
+
+                // Enter focused item
+                if (!selected.buttonFocused) {
+                    items[selected.buttonId].action(selected.buttonId);
+                    // setSelected( { id: selected.id, focused: !selected.buttonFocused, childrenSelected: false } );
+                    setSelected(selectedStateHelper({
+                        buttonId: selected.buttonId,
+                        buttonFocused: !selected.buttonFocused,
+                    }));
+                    return;
+                }
+            }
+            
+            if (key.escape) {
+                
+                // Leave the focused item.
+                if (selected.buttonFocused) {
+                    // setSelected( { id: selected.buttonId, focused: false, childrenSelected: false, } );
+                    setSelected(selectedStateHelper({
+                        buttonId: selected.buttonId,
+                        buttonFocused: false,
+                    }));
+                    // Sometimes this component gets unfocused when pressing escape. That's what this is for.
+                    // focus('main-menu');
+                    return;
+                }
+            }
         }
     });
 
@@ -75,12 +249,12 @@ const MainMenu: FC<{ items: Item[], children?: any }> = ({ items, children }) =>
                 row.push(
                     <Box key={`box-${j}`}
                          borderStyle='round'
-                         borderColor={selected.id === j && isFocused ? 'yellow' : 'white'}
-                         width={ selected.focused ? '100%' : 21}
-                         display={ selected.focused && selected.id !== j ? 'none' : 'flex' }
+                         borderColor={selected.buttonId === j && selected.buttonBoxFocused ? 'yellow' : 'white'}
+                         width={ selected.buttonFocused ? '100%' : 21}
+                         display={ selected.buttonFocused && selected.buttonId !== j ? 'none' : 'flex' }
                          flexDirection='row'
                         >
-                        { selected.focused && selected.id === j ? items[j].element : <Text>{`${items[j].label}`}</Text> }
+                        { selected.buttonFocused && selected.buttonId === j ? items[j].element : <Text>{`${items[j].label}`}</Text> }
                     </Box>
                 );
             }
@@ -96,11 +270,23 @@ const MainMenu: FC<{ items: Item[], children?: any }> = ({ items, children }) =>
         return mapped;
     }
 
+    const addStateToChildren = (children) => {
+        // Mutiple children
+        if (Array.isArray(children)) {
+            // Return children with selected state as prop
+            return children.map((child, id) => React.cloneElement(child, { key: id, isFocused: selected.childId === id && selected.childBoxFocused }));
+        }
+        // Return child with selected state as prop
+        return React.cloneElement(children, { isFocused: selected.childBoxFocused });
+    }
+
+    console.log(selected)
+
     return (
         <Box width='100%' flexDirection="column">
             {/* Children components (aka stuff you do not want inside the button rows) */}
             <Box flexDirection='column'>
-                { children }
+                { addStateToChildren(children) }
             </Box>
 
             {/* mapped items */}
